4a6c4d8f3d037d9bed5746f062501356ffd96617
Dron <gribikc@yandex.ru>
2019-10-25 23:39:30 +0400

    rdc

--- arduino/temperature_logger/temperature_logger/temperature_logger.ino
+++ arduino/temperature_logger/temperature_logger/temperature_logger.ino
@@ -5,10 +5,7 @@
 #include <Adafruit_BMP085_U.h>
 
   #define SD_cs 10
-  
-  Sd2Card card;
-  SdVolume volume;
-  SdFile root;
+  File dataFile;
   
   Stream* user_stream=&Serial;
   Stream* dev_stream=&Serial;
@@ -32,14 +29,10 @@
       SPI.setBitOrder(MSBFIRST);
       SPI.transfer(0);
     //SD
-      init_sd();
+      SD.begin(SD_cs);
+      dataFile = SD.open("datalog.dat", FILE_WRITE);
     //BMP180
-      if(!bmp.begin()) {/* Initialise the sensor */
-        /* There was a problem detecting the BMP085 ... check your connections */
-        Serial.print("Ooops, no BMP085 detected ... Check your wiring or I2C ADDR!");
-        while(1);
-      }
-      displaySensorDetails();/* Display some basic information on this sensor */
+      bmp.begin();
   
     Serial.println("LOOP START:");
     Serial1.println("LOOP START:");
@@ -51,71 +44,40 @@
     bmp180_read();
     delay(1000);
   }
-//////////////////
-/////////////////
-////////////////
-  void init_sd(){
-    if (!card.init(SPI_HALF_SPEED, SD_cs)) {
-      user_stream->println("SD_init...Fail");
-    } else {
-      user_stream->println("SD_init...Done");
-      if (volume.init(card)) {
-        user_stream->println("SD_volume...Done");
-      }else{
-        user_stream->println("SD_volume...FALSE");  
-      }
-    }  
-    uint32_t volumesize;
-  
-    volumesize = volume.blocksPerCluster();    // clusters are collections of blocks
-    volumesize *= volume.clusterCount();       // we'll have a lot of clusters
-    volumesize /= 2;                           // SD card blocks are always 512 bytes (2 blocks are 1KB)
-    volumesize /= 1024;
-    user_stream->print("SD_size (Gb):  ");
-    user_stream->println((float)volumesize / 1024.0);      
-  }
 ////////////////////////
 ///////////////////////
 //////////////////////
-  void displaySensorDetails(void){
-    sensor_t sensor;
-    bmp.getSensor(&sensor);
-    user_stream->println("------------------------------------");
-    user_stream->print  ("Sensor:       "); user_stream->println(sensor.name);
-    user_stream->print  ("Driver Ver:   "); user_stream->println(sensor.version);
-    user_stream->print  ("Unique ID:    "); user_stream->println(sensor.sensor_id);
-    user_stream->print  ("Max Value:    "); user_stream->print(sensor.max_value); Serial.println(" hPa");
-    user_stream->print  ("Min Value:    "); user_stream->print(sensor.min_value); Serial.println(" hPa");
-    user_stream->print  ("Resolution:   "); user_stream->print(sensor.resolution); Serial.println(" hPa");  
-    user_stream->println("------------------------------------");
-    user_stream->println("");
-  }
-  ///////////////////
   void bmp180_read(){
-      /* Get a new sensor event */ 
+    if (!dataFile) {
+      user_stream->print("ERROR!!!");
+    }
+    /* Get a new sensor event */ 
     sensors_event_t event;
     bmp.getEvent(&event);
    
     /* Display the results (barometric pressure is measure in hPa) */
     if (event.pressure){
-      /* Display atmospheric pressue in hPa */
-      user_stream->print("Pressure:    ");
-      user_stream->print(event.pressure);
-      user_stream->println(" hPa");
-      
       float temperature;
-      bmp.getTemperature(&temperature);
-      user_stream->print("Temperature: ");
-      user_stream->print(temperature);
-      user_stream->println(" C");
-  
       float seaLevelPressure = SENSORS_PRESSURE_SEALEVELHPA;
-      user_stream->print("Altitude:    "); 
-      user_stream->print(bmp.pressureToAltitude(seaLevelPressure,event.pressure)); 
-      user_stream->println(" m");
-      user_stream->println("");
+      
+      user_stream->println("xdstartjson:{");                                        dataFile.print("xdstartjson:{");
+      
+      user_stream->print("    \"Pressure\":\"");                                    dataFile.print("    \"Pressure\":\"");
+      user_stream->print(event.pressure);                                           dataFile.print(event.pressure);
+      user_stream->print("\",\n");                                                  dataFile.print("\",\n");
+      bmp.getTemperature(&temperature);                                             //dataFile.print(&temperature);
+      user_stream->print("    \"Temperature\":\"");                                 dataFile.print("    \"Temperature\":\"");
+      user_stream->print(temperature);                                              dataFile.print(temperature);
+      user_stream->print("\",\n");                                                  dataFile.print("\",\n");
+      user_stream->print("    \"Altitude\":\"");                                    dataFile.print("    \"Altitude\":\"");
+      user_stream->print(bmp.pressureToAltitude(seaLevelPressure,event.pressure));  dataFile.print(bmp.pressureToAltitude(seaLevelPressure,event.pressure));
+      user_stream->print("\"\n");                                                   dataFile.print("\"\n");
+      
+      user_stream->print("}:xdstopjson");                                           dataFile.print("}:xdstopjson");
     }else{
-      user_stream->println("Sensor error");
+      user_stream->println("xdstartjson:{");
+      user_stream->println("    \"Sensor error\":\"1\"");
+      user_stream->print("}:xdstopjson");
     }
   }
 ////////////////////////
---
+++ raspberrypi/html/htdocs/arduserv/temperature_logger/temperature_logger_js_xhr_lib.js
@@ -0,0 +1,125 @@
+let nmea;// = new nmea_gr();
+let datafall;// = new datafall_gr();
+let sys_data;//=new xmlhttprq_stream_gr();
+let message_db;
+
+let test_paper;
+
+//var xhr;// = new XMLHttpRequest();
+//var xhr_read_point=0;
+//var xhr_date = new Date;
+//var xhr_fps=0;
+//var xhr_bps=0;
+//var xhr_temp;
+
+var sys_data_param;
+var test_cnt_stream_param;
+
+var autoboat;
+	var test=0;
+
+
+///////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////	
+	function next_prev_main_wiev_div_in(rf){
+		var i;
+		var inner;
+		var inner_count=document.getElementById("main_wiev_div").childElementCount;
+		var vis_ch_tg=0;
+		for(i=0;i<inner_count;i++){
+			//document.getElementById("main_wiev_div").children[i].
+			inner=document.getElementById("main_wiev_div").children[i];
+			if(vis_ch_tg==1){
+				inner.style.position="absolute";
+				inner.style.visibility="hidden";
+				inner.style.zIndex=-1;
+			}else if(inner.style.visibility=="visible" || inner.style.position=="unset" ||
+					 inner.style.zIndex=="unset"       || inner.style.visibility=="" ||
+					 inner.style.position==""          || inner.style.zIndex==""){
+				inner.style.position="absolute";
+				inner.style.visibility="hidden";
+				inner.style.zIndex=-1;
+				
+				//console.log(i);
+				
+				vis_ch_tg=1;
+				if(rf=='f'){
+					if( !(inner=document.getElementById("main_wiev_div").children[i+1]) ){
+						inner=document.getElementById("main_wiev_div").children[0];
+					}
+					i++;
+				}else{
+					if( !(inner=document.getElementById("main_wiev_div").children[i-1]) ){
+						inner=document.getElementById("main_wiev_div").children[inner_count-1];
+						inner_count--;
+					}
+				}
+				
+				vis_ch_tg=1;
+				inner.style.visibility="visible";
+				inner.style.position="unset";
+				inner.style.zIndex="unset";
+			}
+		}
+	}
+///////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////
+	function test_send_data1(){
+		xmlhttprq_test = new XMLHttpRequest();
+		xmlhttprq_test.open('POST', 'http://192.168.0.100:3128/dev/bt/w/grardusand/', true);//, true
+		xmlhttprq_test.overrideMimeType('text/plain; charset=x-user-defined');
+		xmlhttprq_test.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+		var uint8 = new Uint8Array(8);
+		uint8[0] = 72;
+		uint8[1] = 69;
+		uint8[2] = 76;
+		uint8[3] = 76;
+		uint8[4] = 79;
+		uint8[5] = 33;
+		uint8[6] = 33;
+		uint8[7] = 33;
+		xmlhttprq_test.send(uint8);
+		//!!!xmlhttprq_test.abort();
+		//xmlhttprq_test.send();
+	}	
+///////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////
+//$(document).ready(function(){
+function main_init(){
+
+	//HUB
+
+		//autoboat
+		simple_test_gr=new simple_test_gr();
+	//_HUB
+	//STREAM
+		//arduino_uart
+			simple_test_stream_param={//location.hostname
+				url   : (document.location.protocol=="file:" ? "http://127.0.0.1:3128" : "" ) +"/dev/com/r/32/115200", //(document.location.protocol=="file:" ? "http://localhost:3128" : "" ) + '/R/COM/28/57600/',//'http://localhost:3128/R/COM/28/57600/',//'http://192.168.0.122:3128/R/BT/HC-06/',//http://172.20.10.4:3128/R/BT/HC-06///http://192.168.0.122:3128/R/BT/HC-06/',//'/cgi-bin/stream_usart.sh',
+				//url   : (document.location.protocol=="file:" ? "http://127.0.0.1:3128" : "" ) +"/dev/bt/r/grardusand/",
+				//url:"http://192.168.0.100:3128/dev/bt/r/grardusand/",
+				url_w : "127.0.0.1:3128/W/GPS/",//(document.location.protocol=="file:" ? "http://localhost:3128" : "" ) + '/W/COM/28/57600/',//'http://localhost:3128/W/COM/28/57600/',
+				mime_type:'text/plain; charset=x-user-defined',
+				status_div_name:"SP:",
+				//parser: new json_stream_parser_gr(simple_test_gr),//new raw_parser_gr(message_hub),
+				  parser: new json_stream_parser_gr(simple_test_gr),
+				
+				flush_en:true,
+				auto_start:true,
+				
+				status_en:true,
+				status_timer:1000,
+				status_div:"xhr_status_div",
+				status_div_status_css:"xmlhttprq_stream_gr_status",
+				status_div_stat_css:"xmlhttprq_stream_gr_stat",
+				
+				reload_en:true,
+				reload_time:1000
+			};
+			new xmlhttprq_stream_gr(simple_test_stream_param);//'/cgi-bin/test_counter.sh',test_cnt_nmea,"xhr_status_div","TSTCNT:");//14*8*1=112
+		//_arduino_uart
+	///////////////////
+}
